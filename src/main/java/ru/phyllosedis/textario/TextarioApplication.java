3e765ed0cfb16737261d362f84a505e6de8e40d6
phyllosedis <phyllosedis@yandex.ru>
2026-08-25 15:07:57 +0300

    init

---
+++ src/main/java/ru/phyllosedis/textario/TextarioApplication.java
@@ -0,0 +1,13 @@
+package ru.phyllosedis.textario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/phyllosedis/textario/TextarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.phyllosedis.textario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
