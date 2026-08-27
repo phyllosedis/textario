84bf2b8073f4980fa2793d4d72a4536fd6303c6e
phyllosedis <phyllosedis@yandex.ru>
2026-08-27 12:39:35 +0300

    Добавлены тесты на соответствие системе обработки

--- src/test/java/ru/phyllosedis/textario/component/ComponentManagerTest.java
+++ src/test/java/ru/phyllosedis/textario/component/ComponentManagerTest.java
@@ -3,19 +3,24 @@ package ru.phyllosedis.textario.component;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.phyllosedis.textario.component.factory.ComponentFactoryManager;
 import ru.phyllosedis.textario.component.impl.inventory.InventoryComponent;
+import ru.phyllosedis.textario.component.impl.meta.building.BuildingComponent;
 import ru.phyllosedis.textario.component.impl.meta.logistic.transport.impl.InserterComponent;
 import ru.phyllosedis.textario.component.impl.meta.marker.station.OperationFinishedMarkerComponent;
 import ru.phyllosedis.textario.component.impl.meta.marker.tier.TierComponent;
+import ru.phyllosedis.textario.component.impl.mining.MiningComponent;
 import ru.phyllosedis.textario.component.impl.position.PositionComponent;
 import ru.phyllosedis.textario.type.ResourceType;
 import ru.phyllosedis.textario.type.Tier;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -125,6 +130,64 @@ public class ComponentManagerTest {
         assertFalse(
                 cm.matchesSystemFilter(entityId, required),
                 "Сущность НЕ должна подходить системе, так как отсутствует OperationFinishedMarkerComponent, несмотря на наличие лишних компонентов"
+        );
+    }
+
+    @Test
+    @DisplayName("Добытчик НЕ должен попадать в кэш системы Манипулятора (у него отсутствуют соответствующие компоненты:  InserterComponent и OperationFinishedMarkerComponent)")
+    void minerMustNotBeAddedToInserterSystemCache() {
+        cm.add(entityId, cfm.create(new PositionComponent.Args(10, 20)));
+        cm.add(entityId, cfm.create(new BuildingComponent.Args(1, 2)));
+        cm.add(entityId, cfm.create(new MiningComponent.Args(ResourceType.UNDEFINED)));
+        cm.add(entityId, cfm.create(new InventoryComponent.Args(5, 1, List.of(new InventoryComponent.ReadableSlot(ResourceType.UNDEFINED, 1)))));
+        cm.add(entityId, cfm.create(new TierComponent.Args(Tier.ONE)));
+
+
+        Set<Class<? extends Component>> inserterRequirements = Set.of(
+                InserterComponent.class,
+                OperationFinishedMarkerComponent.class,
+                TierComponent.class
+        );
+
+        assertFalse(
+                cm.matchesSystemFilter(
+                        entityId,
+                        inserterRequirements
+                ),
+                "Добытчик НЕ должен попадать в кэш системы Манипулятора (у него отсутствуют соответствующие компоненты:  InserterComponent и OperationFinishedMarkerComponent)"
+        );
+    }
+
+    @MethodSource("missingComponents")
+    @DisplayName("Сущность НЕ соответствует системе при удалении любого из обязательных компонентов")
+    @ParameterizedTest(name = "При отсутствии компонента {0} сущность перестаёт соответствовать системе")
+    void entityDoesNotMatchWhenAnyRequiredComponentIsMissing(
+            Class<? extends Component> missingComponent
+    ) {
+        cm.add(entityId, cfm.create(new InserterComponent.Args(1.0, 1, 1)));
+        cm.add(entityId, cfm.create(new OperationFinishedMarkerComponent.Args()));
+        cm.add(entityId, cfm.create(new TierComponent.Args(Tier.ONE)));
+
+        cm.remove(entityId, missingComponent);
+
+        Set<Class<? extends Component>> required = Set.of(
+                InserterComponent.class,
+                OperationFinishedMarkerComponent.class,
+                TierComponent.class
+        );
+
+        assertFalse(
+                cm.matchesSystemFilter(entityId, required),
+                "После удаления " + missingComponent.getSimpleName() +
+                        " сущность перестаёт соответствовать системе, так как не хватает этого компонента"
+        );
+    }
+
+    static Stream<Class<? extends Component>> missingComponents() {
+        return Stream.of(
+                InserterComponent.class,
+                OperationFinishedMarkerComponent.class,
+                TierComponent.class
         );
     }
 }
